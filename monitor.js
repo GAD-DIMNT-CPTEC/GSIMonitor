dfe9e094a4f2b3ab35b37a816876477a5e776392
joaogerd <joao.gerd@gmail.com>
2023-11-17 16:17:55 -0300

    bug fix

--- monitor.js
+++ monitor.js
@@ -15,6 +15,7 @@ async function startApplication() {
   self.pyodide.globals.set("sendPatch", sendPatch);
   console.log("Loaded!");
   await self.pyodide.loadPackage("micropip");
+  await self.pyodide.loadPackage("sqlite3");
   const env_spec = ['markdown-it-py<3', 'https://cdn.holoviz.org/panel/1.1.1/dist/wheels/bokeh-3.1.1-py3-none-any.whl', 'https://cdn.holoviz.org/panel/1.1.1/dist/wheels/panel-1.1.1-py3-none-any.whl', 'pyodide-http==0.2.1', 'holoviews', 'hvplot', 'numpy', 'pandas']
   for (const pkg of env_spec) {
     let pkg_name;
